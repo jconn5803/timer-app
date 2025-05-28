852ffe057508220ae4de2e0c8a6b117aebf12eb1
jconn5803 <jconn5803@hotmail.com>
2025-05-28 23:15:47 +0100

    adds working start/stop/reset buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./styles.css";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [seconds, setSeconds] = useState(0);
 
-  return (
-    <>
+  const startTimer = () => {
+    window.myTimer = setInterval(() =>
+    {setSeconds((seconds) => seconds + 1)}, 1000)
+  };
+  const stopTimer = () => {
+    clearInterval(window.myTimer)
     
-    </>
-  )
+  };
+  const resetTimer = () => {
+    clearInterval(window.myTimer)
+    setSeconds(0)
+  };
+  return (
+    <div className="container">
+      <h1>Timer</h1>
+      <span> {Math.trunc(seconds / 60)} mins </span>
+      <span> {seconds % 60} secs</span>
+      <div>
+        <button onClick={startTimer}>Start</button>
+        <button onClick={stopTimer}>Stop</button>
+        <button onClick={resetTimer}>Reset</button>
+      </div>
+    </div>
+  );
 }
-
-export default App
